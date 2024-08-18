6997884592a3c7e221e95f1f667f67e8b6b0c464
Candyrizz <rishavsingh076@gmail.com>
2024-08-19 00:16:45 +0530

    Game creative

--- Wonder/Game/Game.cpp
+++ Wonder/Game/Game.cpp
@@ -4,6 +4,10 @@
 void Game::Init()
 {
 	cube.setTransform(Box);
+	TransformComponent prop;
+	prop.position = glm::vec3(0.0f,0.75f,0.0f);
+	cone.setTransform(prop);
+	cone.setColor(0.3f,0.5f,0.33f);
 }
 
 void Game::input()
@@ -17,7 +21,11 @@ void Game::update()
 	std::mt19937 rd_machine(rd());
 	std::uniform_real_distribution<float> urd(0.0f, 1.0f);
 	 
-	
+		TransformComponent prop = cone.getTransform();
+		prop.rotateAngle = glfwGetTime() * 40;
+		prop.rotationDir = Vector3D::Up;
+
+		cone.setTransform(prop);
 		if (static_cast<int>(glfwGetTime()) % 2 == 0)
 		{
 			if (!change)
